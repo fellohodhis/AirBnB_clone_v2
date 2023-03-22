639b8471735de7e3851f0d9e0a63e630e02206be
fellohodhis <odhiambof446@gmail.com>
2023-03-21 23:05:21 -0700

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,19 +1,9 @@
--- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test with password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
-
+-- Prepares a MySQL server
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+-- Create user
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Add privileges
+GRANT USAGE ON * . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
